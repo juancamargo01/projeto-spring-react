d3d3844147242a65bad8fdffde63d7cc8af20e7e
Juan Camargo <juan.09.camargo@gmail.com>
2021-09-06 15:11:37 -0300

    imported remove

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -1,4 +1,4 @@
-import ImgDsDark from 'assets/img/ds-dark.svg';
+
 
 
 const Footer = () => {
@@ -6,7 +6,7 @@ const Footer = () => {
         <footer className="footer mt-auto py-3 bg-dark">
             <div className="container">
                 <p className="text-light">App desenvolvido por <a href="https://www.linkedin.com/in/juan-victor-oliveira-camargo-06aaa61b5/" target="_blank" rel="noreferrer">Juan Camargo</a></p>   
-                            
+
                 <p className="text-light"><small><strong>Semana Spring React</strong><br />
                     Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
             </div>
