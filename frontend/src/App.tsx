b6e1255d33a83b4ce0315d85cb4b5bfe96a0c356
Juan Camargo <juan.09.camargo@gmail.com>
2021-09-06 14:11:49 -0300

    Basic static components
    criado os principais componentes estataticos

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,23 @@
+import DataTable from "components/DataTable";
+import Footer from "components/Footer";
+import NavBar from "components/NavaBar";
 
 
 
 function App() {
   return (
-    <div className="text-primary">
-      <h1>Ola mundo</h1>
-    </div>
+    <>
+      <NavBar />
+      <div className="container">
+        <div className="text-primary">
+          <h1> ola Mundo</h1>
+          <DataTable/>
+
+
+        </div>
+      </div>
+      <Footer/>
+    </>
   );
 }
 
---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,18 @@
+import ImgDsDark from 'assets/img/ds-dark.svg';
+
+
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://www.linkedin.com/in/juan-victor-oliveira-camargo-06aaa61b5/" target="_blank" rel="noreferrer">Juan Camargo</a></p>   
+                            
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+
+    );
+}
+
+export default Footer;
